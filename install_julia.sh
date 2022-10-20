ee1d6040e7096c5ae9e937c7b4723e76b42d2863
MosesLiskov <mliskov@mitre.org>
2022-10-20 18:37:17 +0000

    clean up

--- install_julia.sh
+++ install_julia.sh
@@ -5,3 +5,4 @@ wget https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.2-linux-x86_
 tar -xvzf julia-1.8.2-linux-x86_64.tar.gz
 sudo mv julia-1.8.2/ /opt/
 sudo ln -s /opt/julia-1.8.2/bin/julia /usr/local/bin/julia
+rm julia-1.8.2-linux-x86_64.tar.gz
